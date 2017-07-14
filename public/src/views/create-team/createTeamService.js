2179e80430105cb0cbcc1ec273205a13f1935cd4
Karan Kwatra <kwatraka@umich.edu>
2017-07-14 17:06:15 -0500

    Adding procfile

--- public/src/views/create-team/createTeamService.js
+++ public/src/views/create-team/createTeamService.js
@@ -2,7 +2,7 @@ angular.module("app").service("createTeamService", function($http){
 
 	this.createTeam = function(team){
 		return $http.post('/api/createTeam', {team: team}).then(function(response){
-			return response; 
+			return response;
 		})
 	}
 
